5de705108adae89adacaab81ee74846ee6f83292
JohnMichelin <jmichelinphone@gmail.com>
2017-12-21 11:27:09 -0800

    Brute Force Implementation

--- tree-path-sum.js
+++ tree-path-sum.js
@@ -51,19 +51,38 @@ const testTree = {
 };
 
 const hasPathSum = (root, sum) => {
-  // return false if no root
-  // declare result variable
-  // create traverse() function with node and a total as params
-    // assign new total to passed in total and the node's value
-    // if node has a left child
-      // call traverse function on the left child and new total
-    // if node has a right child
-      // call traverse function on the right child and new total
-    // if node is a leaf
-      // if new total equals sum
-        // assign true to result
-  // call traverse function on the root node and zero as total
-  // if result equals true
-    // return true
-  // else return false
+  if (!root || root.val === 'undefined') return false; // return false if no root
+
+  let result; // declare result variable
+
+  let traverse = function(node, total) {
+    // create traverse() function with node and a total as params
+    let newTotal = total + node.val; // assign new total to passed in total and the node's value
+
+    if (node.left) {
+      // if node has a left child
+      traverse(node.left, newTotal); // call traverse function on the left child and new total
+    }
+
+    if (node.right) {
+      // if node has a right child
+      traverse(node.right, newTotal); // call traverse function on the right child and new total
+    }
+
+    if (!node.right && !node.left) {
+      // if node is a leaf
+      if (newTotal == sum) {
+        // if new total equals sum
+        result = true; // assign true to result
+      }
+    }
+  };
+
+  traverse(root, 0); // call traverse function on the root node and zero as total
+  if (result === true) {
+    // if result equals true
+    return true; // return true
+  } else {
+    return false;
+  } // else return false
 };
