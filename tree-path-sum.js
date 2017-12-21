da1556d14e5c590d4b90dd1ec1f05527c1562a5a
JohnMichelin <jmichelinphone@gmail.com>
2017-12-21 10:39:06 -0800

    Parse - Input - data structure

--- tree-path-sum.js
+++ tree-path-sum.js
@@ -1,3 +1,16 @@
 /*
-Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf 
+ * path such that adding up all the values along the path equals the given sum.
+ *
+ * {
+ *  val: number,
+ *  left: null or node,
+ *  right: null or node
+ * }
+ * 
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ *
 */
+
